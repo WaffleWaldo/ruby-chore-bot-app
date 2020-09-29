738483973504b122766ec209f4199f2dab951b63
jrwatson94 <j.watsonreid@gmail.com>
2020-09-28 20:06:03 -0400

    added basic welcome statement

--- app/CLI.rb
+++ app/CLI.rb
@@ -1,1 +1,22 @@
 ##CLI file for storing CLI classes and methods
+require 'tty-prompt'
+
+
+class Welcome
+    ##Print Welcome message to screen
+    def self.welcome_msg
+        puts "Welcome to ChoreBot!"
+        puts "The digital chore wheel"
+    end
+end
+
+class Registration
+
+end
+
+class LogIn
+end
+
+
+class MainMenu
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,7 @@
 require_relative '../config/environment'
+require_relative '../app/CLI.rb'
+
+Welcome.welcome_msg
 
 
 
