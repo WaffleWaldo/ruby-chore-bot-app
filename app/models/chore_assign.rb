dca7238a82674bab3086bc9dc762c622ce90574a
jrwatson94 <j.watsonreid@gmail.com>
2020-09-30 22:05:32 -0400

    fixing randomize bugs

--- app/models/chore_assign.rb
+++ app/models/chore_assign.rb
@@ -29,8 +29,8 @@ class ChoreAssign < ActiveRecord::Base
     ##Helper method for .randomize
     def self.create_chore_assignment(user,chore)
         new_assign = ChoreAssign.create
-        new_assign.user = user
-        new_assign.chore = chore
+        user.chore_assigns << new_assign
+        chore.chore_assigns << new_assign
     end
     
     def self.randomize
@@ -39,16 +39,14 @@ class ChoreAssign < ActiveRecord::Base
         @@all_chores << Chore.all
         @@all_chores.flatten!
 
-        # until @@all_chores.length == 0 do
+        until @@all_chores.length == 0 do
             User.all.each do |user|
-                if user.chores.length < ((Chore.all.length/User.all.length.to_f).ceil)
+                if user.chores.length < ((Chore.all.length/User.all.length.to_f).ceil) && @@all_chores.sample != nil
                     random_chore = @@all_chores.sample
-                    user.chores << random_chore
                     self.create_chore_assignment(user,random_chore)
                     @@all_chores.delete(random_chore)
                 end
             end
-        # end 
-    end
-    
+        end
+    end 
 end
