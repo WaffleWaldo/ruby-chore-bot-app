19405ca88e0a8c2bf120c878b7095472aa64e662
jrwatson94 <j.watsonreid@gmail.com>
2020-10-02 10:27:11 -0400

    cleaning up run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -3,26 +3,3 @@ require_relative '../app/CLI.rb'
 
 Welcome.welcome_msg
 Welcome.register_or_login
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# table = TTY::Table.new(["Name","Chore","Complete?"], [["a1", "a2","a3"], ["b1", "b2","b3"]])
-# puts table.render(:unicode)
-
-
-
